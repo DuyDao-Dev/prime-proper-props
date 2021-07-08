e693cb07aeaf34ffd56c780552f7a5ded5766351
Duy Dao <jimmydao@gmail.com>
2021-07-08 15:08:16 -0500

    GuestForm successfully ported over. SilveWare still having issues

--- src/components/SilverWare/SilverWare.js
+++ src/components/SilverWare/SilverWare.js
@@ -1,18 +1,18 @@
 
-function DinnerSupplies ({guestList}) {
-    console.log('Where are my dinner supplies?',guestList);
+// function DinnerSupplies ({guestList}) {
+//     console.log('Where are my dinner supplies?',guestList);
 
-    return (
-      <>
-        <h2>Dinner Supplies</h2>
-        <div>Spoons: {guestList.length * 2}</div>
-        <div>Forks: {guestList.length * 2}</div>
-        <div>Knives: {guestList.length * 2}</div>
-      </>
-    );
-}
+//     return (
+//       <>
+//         <h2>Dinner Supplies</h2>
+//         <div>Spoons: {guestList.length * 2}</div>
+//         <div>Forks: {guestList.length * 2}</div>
+//         <div>Knives: {guestList.length * 2}</div>
+//       </>
+//     );
+// }
 
-export default DinnerSupplies;
+// export default DinnerSupplies;
 
 
 
