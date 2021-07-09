f21d7894d259c9baa9b4d535c1c70c877b8fbf64
Duy Dao <jimmydao@gmail.com>
2021-07-09 09:08:21 -0500

    Moved the handleSubmit, 2 useStates, and inserted 2 objects into GuestForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,7 @@ import GuestForm from "../GuestForm/GuestForm";
 
 function App() {
   let [guestList, setGuestList] = useState([]);
-  let [newGuestName, setNewGuestName] = useState('');
-  let [newGuestMeal, setNewGuestMeal] = useState('false');
+
 
   //On load, get guests
   useEffect(() => {
@@ -29,41 +28,37 @@ function App() {
   }
 
 
-  const addGuest = () => {
-    axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
-      .then(response => {
-        // clear inputs
-        setNewGuestName('');
-        setNewGuestMeal(false);
+  const addGuest = (guest) => {
+    axios
+      .post("/guests", { name: guest.name, kidsMeal: guest.kidsMeal })
+      .then((response) => {
+        // need inputs
 
         getGuests();
       })
-      .catch(err => {
-        alert('Error Adding Guest');
+      .catch((err) => {
+        alert("Error Adding Guest");
         console.log(err);
-      })
+      });
   };
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (newGuestName) {
-      addGuest();
-    }
-    else {
-      alert('The new guest needs a name!');
-    }
-  }
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (newGuestName) {
+  //     addGuest();
+  //   } else {
+  //     alert("The new guest needs a name!");
+  //   }
+  // };
 
-  console.log(newGuestMeal)
+
   return (
     <div className="App">
       <Header title={"Prim Proper Props"} />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
-      <GuestForm
-        handleSubmit={handleSubmit}
-      />
+      <GuestForm addGuest={addGuest} />
       <GuestList guestList={guestList} />
       <DinnerSupplies guestList={guestList} />
       <Footer />
--- src/components/GuestForm/GuestForm.js
+++ src/components/GuestForm/GuestForm.js
@@ -1,56 +1,63 @@
+import {useState} from 'react';
 
-function GuestForm ({handleSubmit}) {
-    console.log(props);
-  let handleSubmit = props.handleSubmit;
-  let newGuestName = props.newGuestName;
-  let setNewGuestName = props.setNewGuestName;
-  let newGuestMeal = props.newGuestMeal;
-  let setNewGuestMeal = props.setNewGuestMeal;
+function GuestForm({ addGuest }) {
+  console.log(addGuest);
+    let [newGuestName, setNewGuestName] = useState("");
+    let [newGuestMeal, setNewGuestMeal] = useState("false");
 
-    return (
-      <>
-        <h2>Add a new guest</h2>
-        <form onSubmit={handleSubmit}>
-          <label>Name</label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={newGuestName}
-            onChange={(evt) => setNewGuestName(evt.target.value)}
-          />
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (newGuestName) {
+        addGuest({ name: newGuestName, kidsMeal: newGuestMeal });
+      } else {
+        alert("The new guest needs a name!");
+      }
+    };
+
+  return (
+    <>
+      <h2>Add a new guest</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newGuestName}
+          onChange={(evt) => setNewGuestName(evt.target.value)}
+        />
+        <div>
+          Would this guest like a kid's meal?
           <div>
-            Would this guest like a kid's meal?
             <div>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value={true}
-                    checked={newGuestMeal === "true"}
-                    name="kidsMeal"
-                    onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                  />
-                  Yes, this guest would like a Kid's Meal
-                </label>
-              </div>
-              <div>
-                <label>
-                  <input
-                    type="radio"
-                    value={false}
-                    checked={newGuestMeal === "false"}
-                    name="kidsMeal"
-                    onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                  />
-                  No, this guest would not like a Kid's Meal
-                </label>
-              </div>
+              <label>
+                <input
+                  type="radio"
+                  value={true}
+                  checked={newGuestMeal === "true"}
+                  name="kidsMeal"
+                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                />
+                Yes, this guest would like a Kid's Meal
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={false}
+                  checked={newGuestMeal === "false"}
+                  name="kidsMeal"
+                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                />
+                No, this guest would not like a Kid's Meal
+              </label>
             </div>
           </div>
-          <button type="submit">Add Guest</button>
-        </form>
-      </>
-    );
+        </div>
+        <button type="submit">Add Guest</button>
+      </form>
+    </>
+  );
 }
 
 export default GuestForm;
